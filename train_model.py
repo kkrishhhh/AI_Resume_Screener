7bab1b6ca302bba34d77d27aca129ff40ab1e035
Krishna Thakur <krishhhh.work@gmail.com>
2025-06-18 03:08:46 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -4,10 +4,10 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 import os
 
-# Create model folder if it doesn't exist
+
 os.makedirs('model', exist_ok=True)
 
-# Sample training data
+
 data = {
     'resume_text': [
         "Experienced Python developer with Flask knowledge",
@@ -20,15 +20,15 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Vectorize the text
+
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(df['resume_text'])
 
-# Train a classifier
+
 model = LogisticRegression()
 model.fit(X, df['label'])
 
-# Save the model and vectorizer
+
 with open('model/model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
