5abfa8879971ac1ec0764f6f3a48973dfd649c6b
Krishna Thakur <krishhhh.work@gmail.com>
2025-06-18 02:47:41 +0530

    Fix: bind Flask app to dynamic PORT for Render

--- app.py
+++ app.py
@@ -86,6 +86,5 @@ def download_report(filename):
     return send_from_directory(app.config['REPORT_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    # Bind to the PORT environment variable provided by Render
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host='0.0.0.0', port=port)
